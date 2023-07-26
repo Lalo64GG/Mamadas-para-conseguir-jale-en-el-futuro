4f9c4ea9eb676355a88e36e6d14552d621aef07d
Lalo64GG <129311642+Lalo64GG@users.noreply.github.com>
2023-07-26 17:56:39 -0600

    Add files via upload

---
+++ Practica/React/static/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.createRoot( documente.getElementById('root') ).render(
+    <>
+        <App />
+    </>
+)
