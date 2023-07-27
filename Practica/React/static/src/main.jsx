3f69d2155dc3b0458c4c557fa4023eb5a69d10fa
Lalo64GG <129311642+Lalo64GG@users.noreply.github.com>
2023-07-26 18:06:05 -0600

    Delete Practica/React/static directory

--- Practica/React/static/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-
-ReactDOM.createRoot( documente.getElementById('root') ).render(
-    <>
-        <App />
-    </>
-)
